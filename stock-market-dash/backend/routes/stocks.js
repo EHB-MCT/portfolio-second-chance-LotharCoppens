71d37d11291d7d54662e190b9f2d58c5b020f9e3
LotharCoppens <43748462+LotharCoppens@users.noreply.github.com>
2023-08-20 11:49:16 +0200

    add files

--- stock-market-dash/backend/routes/stocks.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const stocksController = require('../controllers/stocksController');
-
-router.get('/realtime', stocksController.getRealTimeData);
-router.get('/historical', stocksController.getHistoricalData);
-router.get('/watchlist', stocksController.getWatchlist);
-
-module.exports = router;
